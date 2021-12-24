f467aaa65f2f3d75b99fe46660e774487920eed6
Bramhesh <Bramhesh@integertel.com>
2021-12-24 13:29:45 +0530

    changes dec2403

--- streamapp.py
+++ streamapp.py
@@ -7,8 +7,8 @@ import hashlib
 import requests
 import json
 from itertools import chain
-import boto3
-from botocore.exceptions import NoCredentialsError
+# import boto3
+# from botocore.exceptions import NoCredentialsError
 pd.options.display.precision = 1
 # from streamlit_disqus import st_disqus
 
@@ -170,20 +170,20 @@ ACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXX'
 SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 
-def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
+# def upload_to_aws(local_file, bucket, s3_file):
+#     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+#                       aws_secret_access_key=SECRET_KEY)
 
-    try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
+#     try:
+#         s3.upload_file(local_file, bucket, s3_file)
+#         print("Upload Successful")
+#         return True
+#     except FileNotFoundError:
+#         print("The file was not found")
+#         return False
+#     except NoCredentialsError:
+#         print("Credentials not available")
+#         return False
 
 
 # uploaded = upload_to_aws('local_file', 'bucket_name', 's3_file_name')
